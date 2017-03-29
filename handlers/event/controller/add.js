5396d93efca65cdc67a2faec24912e8457b4366d
dennis <parasolgroup25@gmail.com>
2017-03-29 06:33:09 -0700

    first commit

--- handlers/event/controller/add.js
+++ handlers/event/controller/add.js
@@ -2,10 +2,11 @@
 
 let mongoose = require('mongoose');
 const Article = require('tutorial').Article;
+const log = require('log')();
 
 exports.get = function *get(next){
 
-    
+    log.debug("title", this.request.title);
 
 
 };
