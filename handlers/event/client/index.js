ac033b4a52d512f3f7d3edb6aeb292faca68c3e9
Jumbot <parasolgroup25@gmail.com>
2017-04-03 08:15:55 +1000

    dropzone

--- handlers/event/client/index.js
+++ handlers/event/client/index.js
@@ -1,7 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render, } from 'react-dom';
+import { createStore, combineReducers, } from 'redux';
+import { Provider, } from 'react-redux';
+import { reducer as formReducer, } from 'redux-form';
+
 import App from './App.jsx';
 
-ReactDOM.render( < App / >,
+const reducer = combineReducers({
+    form: formReducer,
+});
+
+function configureStore(initialState) {
+    return createStore(reducer, initialState);
+}
+
+const store = configureStore({});
+
+render(
+<Provider store={ store }>
+    <App />
+    </Provider>,
     document.getElementById('root')
 );
